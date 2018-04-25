fda77232b8bcca5f944e70d8a17b5387350a642c
Stas'M <x86corez@gmail.com>
2018-04-25 23:25:26 +0300

    Settings: Implement Logout action

--- app/src/main/java/com/example/WiFiPasswordSearcher/SettingsActivity.java
+++ app/src/main/java/com/example/WiFiPasswordSearcher/SettingsActivity.java
@@ -40,7 +40,6 @@ public class SettingsActivity extends Activity {
                     Intent ServerSettingsIntent = new Intent(SettingsActivity.this, ServerSettingsActivity.class);
                     startActivity(ServerSettingsIntent);
                     break;
-
                 case 1: // About
                     Intent AboutInfoIntent = new Intent(SettingsActivity.this, AboutActivity.class);
                     startActivity(AboutInfoIntent);
@@ -51,6 +50,20 @@ public class SettingsActivity extends Activity {
                     {
                         Version.ShowUpdateDialog(SettingsActivity.this);
                     }
+                    break;
+                case 3: // Logout
+                    Settings mSettings = new Settings(getApplicationContext());
+                    mSettings.Reload();
+                    mSettings.Editor.remove(Settings.APP_SERVER_LOGIN);
+                    mSettings.Editor.remove(Settings.APP_SERVER_PASSWORD);
+                    mSettings.Editor.remove(Settings.API_READ_KEY);
+                    mSettings.Editor.remove(Settings.API_WRITE_KEY);
+                    mSettings.Editor.remove(Settings.API_KEYS_VALID);
+                    mSettings.Editor.commit();
+
+                    Intent StartPage = new Intent(getApplicationContext(), StartActivity.class);
+                    StartPage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(StartPage);
                     break;
             }
         }
