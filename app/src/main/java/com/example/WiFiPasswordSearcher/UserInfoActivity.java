417f74e8a879acc57f3835f8dc53081ba0e2ca8e
Stas'M <x86corez@gmail.com>
2018-05-04 15:03:48 +0300

    UserInfoActivity: Properly format date and time

--- app/src/main/java/com/example/WiFiPasswordSearcher/UserInfoActivity.java
+++ app/src/main/java/com/example/WiFiPasswordSearcher/UserInfoActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by пк on 20.12.2015.
  */
 public class UserInfoActivity extends Activity {
-
-    private Settings mSettings = null;
-    private UserManager User = null;
 
     public TextView txtLogin = null;
     public TextView txtRegDate = null;
@@ -20,17 +22,23 @@ public class UserInfoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user);
 
-        mSettings = new Settings(getApplicationContext());
-
-        User = new UserManager(getApplicationContext());
+        UserManager User = new UserManager(getApplicationContext());
         User.getFromSettings();
 
         txtLogin = (TextView) findViewById(R.id.txtLogin);
         txtRegDate = (TextView) findViewById(R.id.txtRegDate);
         txtGroup = (TextView) findViewById(R.id.txtGroup);
 
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        Date date;
+        try {
+            date = format.parse(User.RegDate);
+        } catch (Exception e) {
+            date = new Date();
+        }
+
         String Nick = User.NickName;
-        String RegDate = User.RegDate;
+        String RegDate = DateFormat.getDateTimeInstance().format(date);
         String Group = User.GetGroup();
 
         txtLogin.setText(Nick);
